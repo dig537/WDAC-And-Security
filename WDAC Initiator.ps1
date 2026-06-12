1a558bd27917976029335f0a015a92ede3f25509
dig537 <134211658+dig537@users.noreply.github.com>
2026-06-12 12:33:46 +0300

    Updated WDAC Initiator.ps1 so it no longer accidentally deletes microsoft policies

--- WDAC Initiator.ps1
+++ WDAC Initiator.ps1
@@ -1,10 +1,11 @@
-﻿Initialize--WDACPolicy
+Save--MyPC
+Initialize--WDACPolicy
 
 # Существующие параметры:
 
 echo "######################################################"
 Prepare--DevsForWDACPolicy -DenyDevelopers @("YANDEX LLC") -SampleFile "C:\WDAC\Samples\Yandex.exe"
-Prepare--NamesForWDACPolicy -DenyNames @("bcdedit.exe", "mracsvc.exe", "mracdrv1.sys")
+Prepare--NamesForWDACPolicy -DenyNames @("mracsvc.exe", "mracdrv1.sys")
 Prepare--DevsForWDACPolicy -DenyDevelopers @("VK Play LLC") -SampleFile "C:\WDAC\Samples\VKPlayLoader_4a36b8f180deb2daf6dfb24976b613dd.exe"
 
 # Новые параметры (после развёртки переместить в существующие):
